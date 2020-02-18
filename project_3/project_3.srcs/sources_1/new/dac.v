6f4b5ece90df53908df7e6979929b4279cd22407
hamayelq <52521315+hamayelq@users.noreply.github.com>
2020-02-18 11:43:24 -0500

    Update dac.v

--- project_3/project_3.srcs/sources_1/new/dac.v
+++ project_3/project_3.srcs/sources_1/new/dac.v
@@ -78,3 +78,68 @@ always @ (posedge sck)
         endcase
 
 endmodule
+
+//use this one instead?
+
+`timescale 1ns / 1ps
+
+module DAC(
+    input SCLK,
+    input CS,
+    input reset,
+    output reg OUT
+
+    );
+
+    reg [15:0] data;
+
+    parameter [3:0] s0 = 0,
+                    s1 = 1,
+                    s2 = 2,
+                    s3 = 3,
+                    s4 = 4,
+                    s5 = 5,
+                    s6 = 6,
+                    s7 = 7,
+                    s8 = 8,
+                    s9 = 9;
+
+    reg [4:0] currState;
+    reg [4:0] nextState;
+
+    reg shiftState; //binary shift yes or no
+
+    always @(posedge SCLK, posedge reset)
+        if(reset) begin
+            data <= 16b'0;
+            currState <= s0;
+            nextState <= s0;
+            shiftState <= 1'b0;
+            OUT <= 1'b0;
+        end
+
+        else if(CS == 0) begin
+            shiftState <= 1'b0;
+            OUT <= data[15];
+            data <= {data[14:0], 1'b0}
+        end
+
+        else if (CS == 1) begin
+            currState <= nextState;
+            if(!shiftState)
+                case(currState)
+                    s0: begin
+                        data <= {8'b0, 8'b0};
+                        nextState <= s1;
+                        shiftState <= 1'b1;
+                        end
+                    s1: begin
+                        data <= {8'b0, 8'b00011000};
+                        nextState <= s2;
+                        shiftState <= 1'b1;
+                        end
+                    s2: begin
+                        data <= {8'b0, 8'b00110010};
+                        nextState <= s3;
+                        shiftState <= 1'b1;
+                
